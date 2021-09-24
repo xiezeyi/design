93e02ad651b0ff45b8fc984798721d96a611a8e8
xiezeyi <394337123@qq.com>
2021-09-24 20:06:35 +0800

    [design-behavioral]template 模板方法

---
+++ src/main/java/com/xzy/design/behavioral/template/MainTest.java
@@ -0,0 +1,12 @@
+package com.xzy.design.behavioral.template;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        AutoCookMachine cookMachine = new AutoCookMachine();
+
+        cookMachine.cook();
+
+    }
+}
