113283f281866118cc3af6300e3069253049a449
xiezeyi <394337123@qq.com>
2021-09-24 21:09:02 +0800

    [design-behavioral]strategy 策略模式

---
+++ src/main/java/com/xzy/design/behavioral/strategy/GameStrategy.java
@@ -0,0 +1,10 @@
+package com.xzy.design.behavioral.strategy;
+
+/**
+ * 游戏策略
+ */
+public interface GameStrategy {
+
+    //战斗策略
+    void warStrategy();
+}
---
+++ src/main/java/com/xzy/design/behavioral/strategy/RandomStrategy.java
@@ -0,0 +1,8 @@
+package com.xzy.design.behavioral.strategy;
+
+public class RandomStrategy implements GameStrategy{
+    @Override
+    public void warStrategy() {
+        System.out.println("大乱斗...");
+    }
+}
